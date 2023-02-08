499f3246864bf0dd7a8c717d433e72301bcc13bd
rabrennie <rennierab@gmail.com>
2023-02-08 20:34:31 +0000

    Fix spinner not loading correctly

--- frontend/src/lib/components/Spinner/Spinner.svelte
+++ frontend/src/lib/components/Spinner/Spinner.svelte
@@ -1,17 +1,14 @@
 <script lang="ts">
-	import anime from 'animejs/lib/anime.es.js';
-	import { createEventDispatcher } from 'svelte';
+	import { browser } from '$app/environment';
+	import { createEventDispatcher, onMount } from 'svelte';
 
-	export let items: string[] = [];
-	export let target: number = 0;
-
-	let myEl: HTMLDivElement;
-
-	const dispatch = createEventDispatcher();
-
-	function spin(): any {
-		if (myEl && items.length) {
-			anime({
+    let spin = () => {};
+	onMount(async () => {
+		const anime = await import('animejs/lib/anime.js');
+		spin = () => {
+		if (browser && myEl && items.length) {
+            console.log(anime);
+			anime.default({
 				targets: myEl,
 				rotate:
 					1080 -
@@ -25,33 +22,43 @@
 			});
 		}
 	}
+	});
 
-	$: myEl && spin();
+	export let items: string[] = [];
+	export let target: number = 0;
+
+	let myEl: HTMLDivElement;
+
+	const dispatch = createEventDispatcher();
+
+	$: browser && myEl && spin();
 </script>
 
 <div class="relative">
-    <div class="absolute right-0 top-1/2 w-4 h-1 border-solid border-r-red-400 border-r-8 border-y-transparent border-y-8 border-l-0 -translate-y-1/2"></div>
-    <div
-	class="relative border-slate-100 border rounded-full p-16"
-	style="min-width: 500px; min-height: 500px; max-height: 500px; max-width: 500px;"
-	bind:this={myEl}
->
-	{#each items as item, i}
-		<div
-			class="pr-8 pl-16 whitespace-nowrap text-ellipsis overflow-hidden"
-			style="width: 250px;
+	<div
+		class="absolute right-0 top-1/2 w-4 h-1 border-solid border-r-red-400 border-r-8 border-y-transparent border-y-8 border-l-0 -translate-y-1/2"
+	/>
+	<div
+		class="relative border-slate-100 border rounded-full p-16"
+		style="min-width: 500px; min-height: 500px; max-height: 500px; max-width: 500px;"
+		bind:this={myEl}
+	>
+		{#each items as item, i}
+			<div
+				class="pr-8 pl-16 whitespace-nowrap text-ellipsis overflow-hidden"
+				style="width: 250px;
     text-align: right;
     position: absolute;
     top: 226px;
     left: 250px;
     transform-origin: center left;
     transform: rotate({(i / items.length) * 360}deg);"
-		>
-			{item}
-		</div>
-		<div
-			class="bg-slate-100"
-			style="width: 250px;
+			>
+				{item}
+			</div>
+			<div
+				class="bg-slate-100"
+				style="width: 250px;
         height: 1px;
         text-align: right;
         position: absolute;
@@ -59,7 +66,7 @@
         left: 249px;
         transform-origin: center left;
         transform: rotate({(i / items.length) * 360 + ((1 / items.length) * 360) / 2}deg);"
-		/>
-	{/each}
-</div>
+			/>
+		{/each}
+	</div>
 </div>
