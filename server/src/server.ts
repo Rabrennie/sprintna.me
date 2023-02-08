db9610a8f11e36a90bd52eac5af5e2499455f98e
rabrennie <rennierab@gmail.com>
2023-02-08 00:12:42 +0000

    Update server to create/update rooms and choices in db

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,7 @@ import ServerToClientEvents from '../../common/ServerToClientEvents';
 import Room, { RoomState } from '../../common/Room';
 import crypto from 'crypto';
 import User from '../../common/User';
+import { db } from './db';
 
 export interface InterServerEvents {}
 
@@ -51,23 +52,56 @@ io.on('connection', (socket) => {
         socket.disconnect();
     }
 
-    socket.on('room:create', (name, callback) => {
+    socket.on('room:create', async (name, callback) => {
         const id = crypto.randomBytes(16).toString('base64url');
         rooms[id] = {
             name,
             state: RoomState.SELECTING,
             id,
-            users: [socket.data.user as User],
+            users: [socket.data.user as User, { id: 'aaaa', name: 'aaaa' }, { id: 'bbbb', name: 'bbbb' }],
             choices: {},
         };
+        await db.rooms.create({
+            data: {
+                link_id: id,
+                name,
+                created_at: new Date(),
+            },
+        });
+
         socket.join(id);
         callback(rooms[id]);
     });
 
-    socket.on('room:join', (roomId, callback) => {
+    socket.on('room:join', async (roomId, callback) => {
         const room = rooms[roomId];
 
         if (!room) {
+            const dbRoom = await db.rooms.findFirst({ where: { link_id: roomId }, include: { choices: true } });
+            if (!dbRoom) {
+                return;
+            }
+            rooms[roomId] = {
+                id: roomId,
+                name: dbRoom.name,
+                state: [RoomState.SELECTING, RoomState.ELIMINATING, RoomState.FINISHED][dbRoom.step],
+                users: dbRoom.choices.map((c) => ({ id: c.user_name, name: 'Anonymous' })),
+                choices: Object.fromEntries(
+                    dbRoom.choices.map((c) => [
+                        c.user_name,
+                        {
+                            user: c.user_name,
+                            choice: {
+                                artist: c.album_artist,
+                                title: c.album_name,
+                                imageUrl: c.album_image,
+                                url: `https://open.spotify.com/album/${c.album_id}`,
+                            },
+                            eliminated: c.eliminated,
+                        },
+                    ]),
+                ),
+            };
             return;
         }
 
@@ -81,7 +115,7 @@ io.on('connection', (socket) => {
         callback(rooms[roomId]);
     });
 
-    socket.on('room:album:select', (roomId, album, callback) => {
+    socket.on('room:album:select', async (roomId, album, callback) => {
         const room = rooms[roomId];
 
         if (room.state == RoomState.SELECTING) {
@@ -90,13 +124,48 @@ io.on('connection', (socket) => {
                 choice: album,
                 eliminated: false,
             };
+
+            const dbRoom = await db.rooms.findFirst({ where: { link_id: roomId } });
+            if (!dbRoom) {
+                return;
+            }
+
+            const data = {
+                room_id: dbRoom.id,
+                user_name: (socket.data.user as User).id,
+                album_id: album.url.split('/')[album.url.split('/').length - 1],
+                album_image: album.imageUrl,
+                album_artist: album.artist,
+                album_name: album.title,
+                created_at: new Date(),
+                eliminated: false,
+            };
+
+            const dbChoice = await db.choices.findFirst({
+                where: {
+                    room_id: dbRoom.id,
+                    user_name: (socket.data.user as User).id,
+                },
+            });
+
+            if (dbChoice) {
+                await db.choices.update({
+                    where: { id: dbChoice.id },
+                    data,
+                });
+            } else {
+                await db.choices.create({
+                    data,
+                });
+            }
+
             io.to(roomId).emit('room:choices:update', roomId, room.choices);
         }
 
         callback();
     });
 
-    socket.on('room:album:eliminate', (roomId, callback) => {
+    socket.on('room:album:eliminate', async (roomId, callback) => {
         const room = rooms[roomId];
 
         const choices = Object.values(room.choices).filter((c) => !c.eliminated);
@@ -104,6 +173,27 @@ io.on('connection', (socket) => {
         if (choices.length >= 2) {
             const choice = choices[crypto.randomInt(choices.length)];
             choice.eliminated = true;
+            const dbRoom = await db.rooms.findFirst({ where: { link_id: roomId } });
+            if (!dbRoom) {
+                return;
+            }
+            const dbChoice = await db.choices.findFirst({
+                where: {
+                    room_id: dbRoom.id,
+                    user_name: (socket.data.user as User).id,
+                },
+            });
+            if (!dbChoice) {
+                return;
+            }
+            await db.choices.update({
+                where: {
+                    id: dbChoice.id,
+                },
+                data: {
+                    eliminated: true,
+                },
+            });
             io.to(roomId).emit('room:album:eliminated', roomId, choice);
         }
 
