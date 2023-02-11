509042804e61dd91553a6f0ef3b4b1a70267a005
rabrennie <rennierab@gmail.com>
2023-02-11 21:50:52 +0000

    Update SelectedAlbum component to be able to show confetti

--- src/lib/components/SelectedAlbum/SelectedAlbum.stories.svelte
+++ src/lib/components/SelectedAlbum/SelectedAlbum.stories.svelte
@@ -21,3 +21,14 @@
         artistName: 'Hot Mulligan',
     }}
 />
+<Story
+    name="Confetti"
+    args={{
+        name: 'Someone',
+        albumName: "you'll be fine",
+        albumImageUrl: 'https://i.scdn.co/image/ab67616d00001e02fec2691d20750b512ba92483',
+        albumLink: 'https://open.spotify.com/album/5MutKcn3J1xzbBKOzr1oGI',
+        artistName: 'Hot Mulligan',
+        confetti: true,
+    }}
+/>
--- src/lib/components/SelectedAlbum/SelectedAlbum.svelte
+++ src/lib/components/SelectedAlbum/SelectedAlbum.svelte
@@ -1,47 +1,53 @@
 <script lang="ts">
-	export let name: string;
-	export let albumLink: string = 'https://open.spotify.com/album/5Z9iiGl2FcIfa3BMiv6OIw';
-	export let albumName: string = 'Not Selected';
-	export let artistName: string = 'Not Selected';
-	export let albumImageUrl: string = 'https://sprintna.me//storage/no-album.png';
-	export let eliminated: boolean = false;
+    import Confetti from '$lib/components/Confetti/Confetti.svelte';
 
-	const emojis = ['❌', '👎', '🙅‍♀️', '⛔️', '🙅', '🙅‍♂️', '😭', '🚫', '💀'];
-	const eliminatedEmoji = emojis[Math.floor(Math.random() * emojis.length)];
+    export let name: string;
+    export let albumLink: string = 'https://open.spotify.com/album/5Z9iiGl2FcIfa3BMiv6OIw';
+    export let albumName: string = 'Not Selected';
+    export let artistName: string = 'Not Selected';
+    export let albumImageUrl: string = 'https://sprintna.me//storage/no-album.png';
+    export let eliminated: boolean = false;
+    export let confetti: boolean = false;
 
-	function stringToHslColor(str: string, s: number, l: number) {
-		var hash = 0;
-		for (var i = 0; i < str.length; i++) {
-			hash = str.charCodeAt(i) + ((hash << 5) - hash);
-		}
+    const emojis = ['❌', '👎', '🙅‍♀️', '⛔️', '🙅', '🙅‍♂️', '😭', '🚫', '💀'];
+    const eliminatedEmoji = emojis[Math.floor(Math.random() * emojis.length)];
 
-		var h = hash % 360;
-		return `hsl(${h}, ${s}%, ${l}%)`;
-	}
+    function stringToHslColor(str: string, s: number, l: number) {
+        var hash = 0;
+        for (var i = 0; i < str.length; i++) {
+            hash = str.charCodeAt(i) + ((hash << 5) - hash);
+        }
+
+        var h = hash % 360;
+        return `hsl(${h}, ${s}%, ${l}%)`;
+    }
 </script>
 
 <div class="indicator">
-	<div
-		class="indicator-item indicator-top indicator-center badge text-primary-content"
-		style="background-color: {stringToHslColor(name, 55, 60)}"
-	>
-		{name}
-	</div>
-	<div class="w-52 {eliminated ? 'opacity-50' : ''}">
-		<a href={albumLink} class="group hover:underline">
-			{#if eliminated}
-				<div class="absolute w-52 h-52 flex items-center justify-center">
-					<div class="text-9xl select-none drop-shadow-[0_20px_20px_rgba(0,0,0,1)]">
-						{eliminatedEmoji}
-					</div>
-				</div>
-			{/if}
-			<div
-				class="bg-white bg-cover w-52 h-52 shadow-lg shadow-slate-900 mb-4 group-hover:scale-105 transition-transform album-image"
-				style="background-image: url({albumImageUrl})"
-			/>
-			<div class="text-primary-content">{albumName}</div>
-			<div class="text-base-content text-sm">{artistName}</div>
-		</a>
-	</div>
+    <div
+        class="indicator-item indicator-top indicator-center badge text-primary-content"
+        style="background-color: {stringToHslColor(name, 55, 60)}"
+    >
+        {name}
+    </div>
+    <div class="w-52 {eliminated ? 'opacity-50' : ''}">
+        <a href={albumLink} class="group hover:underline">
+            {#if eliminated}
+                <div class="absolute w-52 h-52 flex items-center justify-center">
+                    <div class="text-9xl select-none drop-shadow-[0_20px_20px_rgba(0,0,0,1)]">
+                        {eliminatedEmoji}
+                    </div>
+                </div>
+            {/if}
+            <Confetti fireOnLoad={confetti}>
+                <div
+                    class="bg-white bg-cover w-52 h-52 shadow-lg shadow-slate-900 mb-4 group-hover:scale-105 transition-transform album-image"
+                    style="background-image: url({albumImageUrl})"
+                />
+            </Confetti>
+
+            <div class="text-primary-content">{albumName}</div>
+            <div class="text-base-content text-sm">{artistName}</div>
+        </a>
+    </div>
 </div>
