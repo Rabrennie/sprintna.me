c1ffc5b908fdafee88b44a3453aaa112eccd35c4
rabrennie <9087372+Rabrennie@users.noreply.github.com>
2023-05-07 22:39:47 +0100

    Update button to accept HTMLButtonAttributes

--- src/lib/components/Button/Button.svelte
+++ src/lib/components/Button/Button.svelte
@@ -1,14 +1,28 @@
 <script lang="ts">
-	export let variant: undefined | 'primary' | 'success' | 'warning' = undefined;
-	export let block: boolean = true;
+    import type { HTMLButtonAttributes } from 'svelte/elements';
+
+    export let variant: undefined | 'primary' | 'success' | 'warning' = undefined;
+    export let block: boolean = true;
+
+    interface $$Props extends HTMLButtonAttributes {
+        variant?: 'primary' | 'success' | 'warning';
+        block?: boolean;
+    }
 
     let variantMap = {
         primary: 'btn-primary',
         success: 'btn-success',
-        warning: 'btn-warning',
-    }
+        warning: 'btn-warning'
+    };
 </script>
 
-<button class="btn {block && 'btn-block'} {variant && variantMap[variant]}" type="button" on:click
-	><slot /></button
+<button
+    on:click
+    {...{
+        ...$$restProps,
+        class: `${$$restProps.class || ''} btn ${block ? 'btn-block' : ''} ${
+            variant ? variantMap[variant] : ''
+        }`,
+        type: $$restProps.type ? $$restProps.type : 'button'
+    }}><slot /></button
 >
