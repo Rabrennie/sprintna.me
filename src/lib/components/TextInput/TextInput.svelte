eeb4da6b5eb87ca2d9d83c88bcd830e9dbd06a53
rabrennie <9087372+Rabrennie@users.noreply.github.com>
2023-05-07 22:39:57 +0100

    Update TextInput to show errors

--- src/lib/components/TextInput/TextInput.svelte
+++ src/lib/components/TextInput/TextInput.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
     import type { HTMLInputAttributes } from 'svelte/elements';
-    interface $$Props extends HTMLInputAttributes {}
+    interface $$Props extends HTMLInputAttributes {
+        errors?: Record<string, string> | undefined;
+    }
 
     export let value: string = '';
+    export let errors: Record<string, string> | undefined = {};
+
+    $: currentError = $$restProps?.name && errors && errors[$$restProps?.name] ? errors[$$restProps.name] : null;
 </script>
 
 <div class="form-control w-full">
@@ -11,7 +16,12 @@
         type="text"
         {...{
             ...$$restProps,
-            class: `${$$restProps.class || ''} input input-bordered w-full`
+            class: `${$$restProps?.class || ''} input input-bordered w-full ${currentError ? 'input-error' : ''}`
         }}
     />
+    {#if currentError}
+        <label class="label" for={$$restProps.name}>
+            <span class="label-text-alt text-error">{currentError}</span>
+        </label>
+    {/if}
 </div>
