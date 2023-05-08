9c19be0aeab7168dc11cf744755a39eb4bd6df8c
rabrennie <9087372+Rabrennie@users.noreply.github.com>
2023-05-08 14:01:25 +0100

    Add ghost variant and loading prop

--- src/lib/components/Button/Button.svelte
+++ src/lib/components/Button/Button.svelte
@@ -1,19 +1,22 @@
 <script lang="ts">
     import type { HTMLButtonAttributes } from 'svelte/elements';
 
-    export let variant: undefined | 'primary' | 'success' | 'warning' = undefined;
-    export let block: boolean = true;
+    export let variant: $$Props['variant'] = undefined;
+    export let block: $$Props['block'] = true;
+    export let loading: $$Props['block'] = false;
 
-    interface $$Props extends HTMLButtonAttributes {
-        variant?: 'primary' | 'success' | 'warning';
-        block?: boolean;
-    }
-
-    let variantMap = {
+    const variantMap = {
         primary: 'btn-primary',
         success: 'btn-success',
-        warning: 'btn-warning'
+        warning: 'btn-warning',
+        ghost: 'btn-ghost'
     };
+
+    interface $$Props extends HTMLButtonAttributes {
+        variant?: keyof typeof variantMap;
+        block?: boolean;
+        loading?: boolean;
+    }
 </script>
 
 <button
@@ -22,7 +25,7 @@
         ...$$restProps,
         class: `${$$restProps.class || ''} btn ${block ? 'btn-block' : ''} ${
             variant ? variantMap[variant] : ''
-        }`,
+        } ${loading ? 'loading' : ''}`,
         type: $$restProps.type ? $$restProps.type : 'button'
     }}><slot /></button
 >
