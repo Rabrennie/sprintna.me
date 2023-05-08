044710f83fb2e954489aca802cdde44e66e3ea5f
rabrennie <9087372+Rabrennie@users.noreply.github.com>
2023-05-08 14:01:57 +0100

    Update Modal to dispatch open/close events and have a customButton slot

--- src/lib/components/Modal/Modal.svelte
+++ src/lib/components/Modal/Modal.svelte
@@ -1,29 +1,37 @@
 <script lang="ts">
-	export let id: string;
+    import { createEventDispatcher } from 'svelte';
 
-	export let open: boolean = false;
-	export let canClose: boolean = true;
+    const dispatch = createEventDispatcher();
 
-	export function toggle() {
-		open = !open;
-	}
+    export let id: string;
+    export let isOpen: boolean = false;
+    export let canClose: boolean = true;
 
-	export let buttonVariant: undefined | 'primary' | 'success' = undefined;
+    export function toggle() {
+        isOpen = !isOpen;
+    }
 
-	let buttonVariantMap = {
-		primary: 'btn-primary',
-		success: 'btn-success'
-	};
+    export let buttonVariant: undefined | 'primary' | 'success' = undefined;
+
+    let buttonVariantMap = {
+        primary: 'btn-primary',
+        success: 'btn-success'
+    };
+
+    $: isOpen ? dispatch('open') : dispatch('close');
 </script>
 
 {#if $$slots.button}
-	<label for={id} class="btn {buttonVariant && buttonVariantMap[buttonVariant]}"
-		><slot name="button" /></label
-	>
+    <label for={id} class="btn {buttonVariant && buttonVariantMap[buttonVariant]}">
+        <slot name="button" />
+    </label>
 {/if}
-<input type="checkbox" {id} class="modal-toggle" bind:checked={open} />
-<label for={canClose ? id: null} class="modal { canClose && 'cursor-pointer'}">
-	<label class="modal-box relative" for="">
-		<slot name="modal" />
-	</label>
+{#if $$slots.customButton}
+    <slot name="customButton" {toggle} />
+{/if}
+<input type="checkbox" {id} class="modal-toggle" bind:checked={isOpen} />
+<label for={canClose ? id : null} class="modal {canClose && 'cursor-pointer'}">
+    <label class="modal-box relative" for="">
+        <slot name="modal" {toggle} />
+    </label>
 </label>
