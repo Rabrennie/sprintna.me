3c7644b8abd4852e95d637d5d94e2b933342b268
Rab Rennie <9087372+Rabrennie@users.noreply.github.com>
2023-05-27 11:55:56 +0100

    Add leaderboard to team page (#7)

---
+++ src/lib/Utils/stringToHslColor.ts
@@ -0,0 +1,9 @@
+export function stringToHslColor(str: string, s: number, l: number) {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+
+    const h = hash % 360;
+    return `hsl(${h}, ${s}%, ${l}%)`;
+}
