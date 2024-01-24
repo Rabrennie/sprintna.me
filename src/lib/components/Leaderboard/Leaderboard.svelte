3efec5a97780e95b3db84aba361a7eaf48ab53fb
Rab Rennie <rennierab@gmail.com>
2024-01-24 22:40:58 +0000

    Remove dependency on layercake for leaderboard (#23)
    Closes #17

--- src/lib/components/Leaderboard/Leaderboard.svelte
+++ src/lib/components/Leaderboard/Leaderboard.svelte
@@ -1,35 +1,68 @@
 <script lang="ts">
-    import Column from '$lib/components/Leaderboard/Column.svelte';
-    import { LayerCake, Svg, Html } from 'layercake';
-    import { scaleBand } from 'd3-scale';
-    import Labels from '$lib/components/Leaderboard/Labels.svelte';
+    import { stringToHslColor } from '$lib/utils/stringToHslColor';
 
     export let data: { name: string; image: string; count: number }[];
+
+    $: maxCount = Math.max(...data.map((d) => d.count));
 </script>
 
-<div class="chart-container">
-    <LayerCake
-        padding={{ top: 0, right: 0, bottom: 45, left: 0 }}
-        {data}
-        x="name"
-        y="count"
-        xScale={scaleBand().paddingInner(0.2).round(true)}
-        xDomain={data.map((d) => d.name)}
-        yDomain={[0, data[0].count]}
-    >
-        <Svg>
-            <Column />
-        </Svg>
-
-        <Html>
-            <Labels />
-        </Html>
-    </LayerCake>
+<div class="chart-container flex flex-col gap-4">
+    <ul class="chart" style="--max-count: {maxCount}">
+        {#each data as d, i}
+            {#if d.count > 0}
+                <li
+                    class="tooltip tooltip-top"
+                    data-tip={`${d.name} - ${d.count}`}
+                    style="--count: {d.count}; --index: {i}; --fill: {d.count > 0
+                        ? stringToHslColor(d.name, 55, 60)
+                        : 'transparent'};"
+                >
+                    {d.count}
+                </li>
+            {/if}
+        {/each}
+    </ul>
+    <ul class="labels">
+        {#each data as d, i}
+            {#if d.count > 0}
+                <li data-tip={`${d.name} - ${d.count}`} style="--count: {d.count}; --index: {i};">
+                    <div class="avatar">
+                        <div class="w-8 rounded-full">
+                            <img src={d.image} alt={d.name} />
+                        </div>
+                    </div>
+                </li>
+            {/if}
+        {/each}
+    </ul>
 </div>
 
 <style>
     .chart-container {
         width: 100%;
-        height: 150px;
+    }
+
+    .chart,
+    .labels {
+        display: grid;
+        grid-auto-rows: 1fr;
+        grid-auto-columns: 1fr;
+        gap: 0 2rem;
+        max-height: 150px;
+    }
+
+    .chart > li {
+        color: transparent;
+        background-color: var(--fill);
+        border-radius: 0.15rem 0.15rem 0 0;
+        grid-row: calc(var(--max-count) - var(--count) + 1) / span var(--count);
+        grid-column: calc(var(--index) + 1);
+    }
+
+    .labels > li {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        grid-column: calc(var(--index) + 1);
     }
 </style>
